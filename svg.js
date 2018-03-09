b9fa6f7a3d0bc4565c5f91946844482e3e2e7692
unknown <lolevsky@stuy.edu>
2018-03-09 00:54:08 -0500

    fin

--- svg.js
+++ svg.js
@@ -1,54 +1,53 @@
+//With help from Jake Zaia's code!
+
 var clear = document.getElementById("clear");
 var svg = document.getElementById("svg");
 
-var circle = function(x, y, color){
-    var c =
-	{x:x,//c.setAttribute("cx", x),
-	 y:y,//c.setAttribute("cy", y),
-	 r:10,
-	 color:color,//c.setAttribute("fill", color),
-	 svg:svg,
-	 display:append,
-	 remove:circleCallback
-	};
+var circle = function(x, y){
+    var c = {
+	x: x,
+	y: y,
+	r: 10,
+	color: "red",
+	svgCircle: document.createElementNS("http://www.w3.org/2000/svg", "circle"),
+	
+	display:  function(e){
+	    c.svgCircle.setAttribute("cx", this.x);
+	    c.svgCircle.setAttribute("cy", this.y);
+	    c.svgCircle.setAttribute("r", this.r);
+	    c.svgCircle.setAttribute("fill", this.color);
+	    svg.append(c.svgCircle);
+	},
+
+	remove: function(){
+	    svg.removeChild(this.svgCircle);
+	},
+	
+	changeState: function(e){
+	    e.stopPropagation();
+	    if(this.color == "red"){
+		this.color = "green";
+		this.display();
+	    }
+	    else{
+		this.remove();
+		draw({offsetX: Math.random() * 500, offsetY: Math.random() * 500});
+	    }
+	}
+    }
+    c.svgCircle.addEventListener("click", function(e){c.changeState(e)});
     return c;
 };
+
 
 var clearCallback = function(e){
     svg.innerHTML = "";
 };
 
-var svgCallback = function(e){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    var x = e.offsetX;
-    var y = e.offsetY;
-
-    var c = circle(x, y, "red");
-    c.setAttribute("cx", c.x);
-    c.setAttribute("cy", c.y);
-    c.setAttribute("fill", c.color);
-};
-
-var append = function(c){
-    c.addEventListener("click", circleCallback);
-    svg.append(c);
-};
-
-var circleCallback = function(e){
-    //e.target.setAttribute("fill", "green");
-    e.target.remove();
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    var x = Math.random() * svg.getAttribute("width");
-    var y = Math.random() * svg.getAttribute("height");
-    c.setAttribute("cx", x);
-    c.setAttribute("cy", y);
-    c.setAttribute("r", "10");
-    c.setAttribute("fill", "red");
-    c.setAttribute("clicked", "0");
-    c.addEventListener("click", circleCallback);
-    svg.appendChild(c);
-    e.stopPropagation();
-};
+var draw = function(e){
+    var newObj = circle(e.offsetX, e.offsetY);
+    newObj.display();
+}
 
 clear.addEventListener("click", clearCallback);
-svg.addEventListener("click", svgCallback);
+svg.addEventListener("click", draw);
